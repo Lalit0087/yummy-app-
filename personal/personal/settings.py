6ded26a7e689c212b0b7b6789c042f3587b16492
Lalit0087 <51874961+Lalit0087@users.noreply.github.com>
2020-01-22 23:56:59 +0530

    Update settings.py

--- personal/personal/settings.py
+++ personal/personal/settings.py
@@ -15,10 +15,7 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-STATIC_ROOT = os.path.normpath(os.path.join(BASE_DIR, 'staticfiles'))
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+
 
 
 # Quick-start development settings - unsuitable for production
@@ -136,5 +133,5 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    'personal\blogs\static',
+    'personal\static',
 ]
